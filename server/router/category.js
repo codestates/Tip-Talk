897e997ab297374f017bada662d8b8a1d9a6c32f
3002 <psch0493@gmail.com>
2021-10-21 11:27:35 +0900

    [Server] chore: router에 주석 추가

--- server/router/category.js
+++ server/router/category.js
@@ -3,10 +3,13 @@ import * as categoryController from '../controller/category.js';
 
 const router = express.Router();
 
+// * /category
 router.get('/', categoryController.getAll);
 
+// ToDo 유저 미들웨어 추가
 router.post('/', categoryController.add);
 
+// ToDo 유저 미들웨어 추가
 router.delete('/:id', categoryController.deleteOne);
 
 export default router;
--- server/router/comment.js
+++ server/router/comment.js
@@ -3,9 +3,16 @@ import * as commentController from '../controller/comment.js';
 
 const router = express.Router();
 
+// * /comment
 router.get('/:postid', commentController.getAll);
+
+// ToDo 유저 미들웨어 추가
 router.post('/:postid', commentController.upload);
+
+// ToDo 유저 미들웨어 추가
 router.patch('/:commentid', commentController.update);
+
+// ToDo 유저 미들웨어 추가
 router.delete('/:commentid', commentController.deleteOne);
 
 export default router;

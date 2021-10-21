9a792a35412e6b3649c45853fa895b76548b80c8
3002 <psch0493@gmail.com>
2021-10-21 11:05:50 +0900

    [Server] feat: category controller 더미데이터로 구현

--- server/controller/category.js
+++ server/controller/category.js
@@ -1,10 +1,47 @@
-const comments = [
+let categories = [
   {
     id: 1,
     value: '여행지',
   },
+  {
+    id: 2,
+    value: '카페',
+  },
 ];
 
 export function getAll(req, res) {
-  res.status(200).json(comments);
+  res.status(200).json({ status: true, data: categories });
+}
+
+export function add(req, res) {
+  const { value } = req.body;
+
+  // ToDo 토큰 확인 후, 역할이 admin인지 확인하기
+  /*
+  if (user.role !== 0) {
+    res
+      .status(403)
+      .json({ status: false, message: '권한이 없는 사용자입니다.' });
+  }
+  */
+
+  const created = {
+    id: Date.now(),
+    value,
+  };
+
+  categories.push(created);
+
+  res.status(201).json({ status: true, data: created });
+}
+
+export function deleteOne(req, res) {
+  const { id } = req.params;
+
+  categories = categories.filter((category) => category.id !== +id);
+
+  // ! 204는 데이터를 보내지 않는 것 같음. API 문서 수정하기
+  res
+    .status(204)
+    .json({ status: true, message: '카테고리 삭제가 성공하였습니다.' });
 }
--- server/router/category.js
+++ server/router/category.js
@@ -4,5 +4,7 @@ import * as categoryController from '../controller/category.js';
 const router = express.Router();
 
 router.get('/', categoryController.getAll);
+router.post('/', categoryController.add);
+router.delete('/:id', categoryController.deleteOne);
 
 export default router;

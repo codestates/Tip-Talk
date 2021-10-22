099fa65216c74d58d6f591c8e5ae0a3d53210dc4
JungSoochang <tnckddl2468@gmail.com>
2021-10-22 15:38:02 +0900

    [Server] feat: userController

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,5 +1,38 @@
+require('dotenv').config();
+const { user } = require('../models');
+const jwt = require('jsonwebtoken');
+
 module.exports = {
-  getUser(req, res) {
-    res.status(200).send('done');
+  getUserInfo: async (req, res) => {
+    const accessToken = req.cookies.accessToken;
+    const { id } = req.params;
+    const { nickname, password, img } = req.body;
+    const userInfo = await user.findOne({ where: { id } });
+    if (!userInfo) {
+      return res
+        .status(400)
+        .json({ status: false, message: '존재하지 않는 유저입니다.' });
+    }
+    return res.status(200).json({
+      status: true,
+      data: { nickname: nickname, password: password, img: img },
+    });
+  },
+  editUserInfo: async (req, res) => {
+    const accessToken = req.cookies.accessToken;
+    const { id } = req.params;
+    const { nickname, password, img } = req.body;
+    const userInfo = await user.update(
+      { nickname, password, img },
+      {
+        where: { id },
+      },
+    );
+    if (!userInfo) {
+      return res
+        .status(400)
+        .json({ status: false, message: '존재하지 않는 유저입니다.' });
+    }
+    return res.status(200).json({ status: true });
   },
 };

553d12fe7cc38148c0d4b3717cd5040c62248898
3002 <psch0493@gmail.com>
2021-10-27 21:48:28 +0900

    [Server] feat: comment/getAll 응답 데이터 수정

--- server/controller/comment.js
+++ server/controller/comment.js
@@ -1,4 +1,4 @@
-const { comments, post } = require('../models');
+const { comments, post, user } = require('../models');
 
 async function getOne(id) {
   return await comments.find((comment) => comment.id === +id);
@@ -6,10 +6,12 @@ async function getOne(id) {
 
 module.exports = {
   getAll: async (req, res) => {
-    const { postid } = req.params;
+    const { postId } = req.params;
 
     try {
-      const foundPost = await post.findOne({ where: { id: postid } });
+      const foundPost = await post.findOne({
+        where: { id: postId },
+      });
 
       if (!foundPost) {
         return res
@@ -17,7 +19,10 @@ module.exports = {
           .json({ status: true, message: '존재하지 않는 게시글입니다.' });
       }
 
-      const data = await comments.findAll({ where: { postid } });
+      const data = await comments.findAll({
+        where: { postId },
+        include: [{ model: user, attributes: ['nickname', 'img'] }],
+      });
       res.status(200).json({ status: true, data });
     } catch (error) {
       res.status(500).json({ status: false, message: error.message });
@@ -50,7 +55,7 @@ module.exports = {
         postId,
       };
 
-      const created = await comments.create({ data });
+      const created = await comments.create({ ...data });
 
       res.status(200).json({ status: true, data: created });
     } catch (error) {
@@ -58,7 +63,7 @@ module.exports = {
     }
   },
   update: async (req, res) => {
-    const { commentid } = req.params;
+    const { commentId } = req.params;
     const { text } = req.body;
 
     // * 텍스트 입력했는지 확인
@@ -70,7 +75,7 @@ module.exports = {
 
     try {
       // * commentid를 이용 comment 찾기
-      const comment = await comments.findOne({ where: { id: commentid } });
+      const comment = await comments.findOne({ where: { id: commentId } });
 
       if (!comment) {
         return res
@@ -80,7 +85,7 @@ module.exports = {
 
       const updated = await comments.update(
         { text },
-        { where: { id: commentid } },
+        { where: { id: commentId } },
       );
 
       res.status(200).json({ status: true, data: updated });
@@ -89,10 +94,10 @@ module.exports = {
     }
   },
   deleteOne: async (req, res) => {
-    const { commentid } = req.params;
+    const { commentId } = req.params;
 
     try {
-      const found = await comments.findOne({ where: { id: commentid } });
+      const found = await comments.findOne({ where: { id: commentId } });
 
       // ToDo 유저 확인하기
       /*
@@ -100,7 +105,7 @@ module.exports = {
       
       }
       */
-      await comments.destroy({ where: { id: commentid } });
+      await comments.destroy({ where: { id: commentId } });
       res.sendStatus(204);
     } catch (error) {
       res.status(500).json({ status: false, message: error.message });

7201b29a09fd658ffe96556a23d9636ab8e8e345
JungSoochang <tnckddl2468@gmail.com>
2021-11-02 14:25:18 +0900

    [Server] feat:post middleware add

--- server/router/post.js
+++ server/router/post.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const multer = require('multer');
-
+const { isLoggedIn, isAuth } = require('../middleware');
 const { postController } = require('../controller/index');
 
 const router = express.Router();
 const upload = multer();
 
 router.get('/:id', postController.getPost);
-router.post('/', upload.array(), postController.uploadPost);
-router.patch('/:id', postController.editPost);
-router.delete('/:id', postController.deletePost);
+router.post('/', isLoggedIn, isAuth, upload.array(), postController.uploadPost);
+router.patch('/:id', isLoggedIn, isAuth, postController.editPost);
+router.delete('/:id', isLoggedIn, isAuth, postController.deletePost);
 
 module.exports.postRouter = router;

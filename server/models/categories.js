d81b407b5b46aa479732895e5acbce6043584824
3002 <psch0493@gmail.com>
2021-11-05 15:15:35 +0900

    [Server] fix: db 관계 수정

--- server/models/categories.js
+++ server/models/categories.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      categories.belongsTo(models.post, { foreignKey: 'id' });
+      categories.hasMany(models.post, { foreignKey: 'id' });
     }
   }
   categories.init(

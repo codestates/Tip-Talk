2ba2c7682b95f57d01b2fabd7b6ca2a620c93127
3002 <psch0493@gmail.com>
2021-11-05 22:27:09 +0900

    [Server] feat: 코멘트 page 구현

--- server/controller/comments/getAll.js
+++ server/controller/comments/getAll.js
@@ -1,6 +1,7 @@
 const { comments, post, user } = require('../../models');
 
 module.exports = async (req, res) => {
+  const { page } = req.query;
   const { postId } = req.params;
 
   try {
@@ -14,9 +15,14 @@ module.exports = async (req, res) => {
         .json({ status: false, message: '존재하지 않는 게시글입니다.' });
     }
 
+    let max = await comments.count();
+    max = Math.ceil(max / 10) - 1;
+
     const data = await comments.findAll({
       where: { postId },
       include: [{ model: user, attributes: ['nickname', 'img'] }],
+      offset: page ? +page * 10 : 0,
+      limit: 10,
     });
 
     if (req.user) {
@@ -27,7 +33,7 @@ module.exports = async (req, res) => {
       });
     }
 
-    res.status(200).json({ status: true, data });
+    res.status(200).json({ status: true, data, max });
   } catch (error) {
     res.status(500).json({ status: false, message: error.message });
   }

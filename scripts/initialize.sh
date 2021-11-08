05a80d10bd3291fdd4f300f15bfa2e34dd028796
3002 <psch0493@gmail.com>
2021-11-08 10:51:17 +0900

    [Client] chore: 캐로셀 스타일 수정

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -7,8 +7,6 @@ export DATABASE_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --name
 export DATABASE_HOST=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_HOST --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_NAME=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_NAME --query Parameters[0].Value | sed 's/"//g')
 npx sequelize-cli db:migrate
-npx sequelize-cli db:seed:undo:all
-npx sequelize-cli db:seed:all
 sudo apt-get update
 sudo apt-get install authbind
 sudo touch /etc/authbind/byport/80
